5df703b84e95d3068eb6a21b47f4a7346c37b987
Thiago Pereira Correa <thiago.correa147@hotmail.com>
2018-12-09 22:57:41 -0200

    final fixes

--- app/src/main/java/com/test/renata/projetorenata/NovaReceitaActivity.java
+++ app/src/main/java/com/test/renata/projetorenata/NovaReceitaActivity.java
@@ -1,13 +1,26 @@
 package com.test.renata.projetorenata;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NovaReceitaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.app_bar_novaReceita);
+        setContentView(R.layout.app_bar_nova_receita);
+
+
+        Button add = findViewById(R.id.button_novareceita);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(NovaReceitaActivity.this,Main.class);
+                startActivity(i);
+            }
+        });
     }
 }
